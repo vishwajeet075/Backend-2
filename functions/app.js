56411c46605e1d0f4943ab22a36775a22f8dd271
vishwajeet075 <158187016+vishwajeet075@users.noreply.github.com>
2024-08-10 09:19:02 +0530

    Update app.js

--- functions/app.js
+++ functions/app.js
@@ -1,9 +1,9 @@
 console.log('Server starting...');
 const serverless = require('serverless-http');
-const { MongoClient } = require('mongodb');
+/*const { MongoClient } = require('mongodb');*/
 const express = require('express');
 const cors = require('cors');
-
+const faunadb = require('faunadb');
 const util = require('util'); 
 
 const fs=require('fs');
@@ -24,10 +24,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+const q = faunadb.query;
+const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
 
 
 
-const uri = process.env.mongo_url;
+app.post('/submit-form-1', async (req, res) => {
+  const { name, email, message } = req.body;
+  try {
+    const result = await client.query(
+      q.Create(
+        q.Collection('contact_mini'),
+        { data: { name, email, message } }
+      )
+    );
+    console.log("Document Created and Inserted: ", result.ref);
+    res.status(200).json({ success: true, message: 'Form submitted successfully' });
+  } catch (error) {
+    console.error('Error submitting form:', error);
+    res.status(500).json({ success: false, message: 'An error occurred' });
+  }
+});
+
+
+/*const uri = process.env.mongo_url;
 const client = new MongoClient(uri);
 
 app.post('/submit-form-1', async (req, res) => {
@@ -46,7 +66,7 @@ app.post('/submit-form-1', async (req, res) => {
   } finally {
     await client.close();
   }
-});
+});*/
 
 
 
